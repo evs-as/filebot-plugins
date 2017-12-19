9bbf4c85cab883f4fa147909c389a341ca5c4ebf
Anton Strilez <as@evait.de>
2017-12-20 00:41:38 +0100

    change to filebot-rpi

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-mkdir -p $PWD/volume1
-docker build --rm -t filebot-rpi -f Dockerfile . && docker run -it -v $PWD:/volume1 -v data:/data filebot-rpi -script fn:sysinfo
+docker build --rm -t filebot-rpi:latest -f Dockerfile . && docker run -it -v $PWD:/volume1 -v data:/data filebot-rpi:latest -script fn:sysinfo
